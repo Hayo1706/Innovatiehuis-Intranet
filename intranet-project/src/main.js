0649bc9f1409c991ebe4a0b73145de28d34aacb5
Jojahoka <jojahoka@gmail.com>
2021-11-24 17:29:07 +0100

    Basic project page start
    Mostly CSS trial

--- intranet-project/src/main.js
+++ intranet-project/src/main.js
@@ -8,12 +8,14 @@ import "bootstrap"
 
 import Allprojects from './pages/adminpages/projects/Allprojects.vue'
 import Test from './pages/test/Test.vue'
+import Project from './pages/test/Project.vue'
 
 
 
 const routes = [
   { path: '/', component: Allprojects },
-  { path: '/test', component: Test }
+  { path: '/test', component: Test },
+  { path: '/project', component: Project }
 ]
 const router = createRouter({
   history: createWebHistory(),
