d2ea83bb056bc8106e946f9b867ab2ac7bb4be49
singh1998 <46492021+singh1998@users.noreply.github.com>
2021-11-24 23:06:37 +0100

    Fixed the header in a header in a header in a header in a header...

--- intranet-project/src/App.vue
+++ intranet-project/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-    <div>
-      <header>
-        <p id="header_title_text">{{ header_title }}</p>
-      </header>
-      <router-view @loaded="setHeaderTitle"></router-view>
-    </div>
+  <div>
+    <app-header v-bind:header_title="this.header_title"></app-header>
+    <router-view @loaded="setHeaderTitle"></router-view>
+  </div>
 </template>
 
 <script>
+import AppHeader from "./shared_components/AppHeader.vue";
 export default {
-  components: {},
+  components: { AppHeader },
   name: "App",
   data: function () {
     return {
@@ -26,22 +25,6 @@ export default {
 </script>
 
 <style>
-header {
-  width: 100%;
-  height: 10vh;
-  background-color: #28418a;
-}
-#header_title_text {
-  color: white;
-  font-size: calc(2vw + 2vh);
-  height: 100%;
-  margin: 0;
-  text-align: center;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-family: Montserrat;
-}
 body {
   margin: 0;
 }
