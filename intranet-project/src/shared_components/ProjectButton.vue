859db975db34f94d659217f338f8242d937bc5c8
Pieter Beens <beens.pieter@gmail.com>
2021-11-30 00:32:16 +0100

    ProjectsWindow praat nu met API

--- intranet-project/src/shared_components/ProjectButton.vue
+++ intranet-project/src/shared_components/ProjectButton.vue
@@ -1,11 +1,11 @@
 <template>
   <button @click="onClick()" id="project-button">
     <img
-      v-if="recentUpdate"
+      v-if="dateStringtoJsDate(lastUpdated) >= lastVisit"
       src=".\..\assets\images\logo\square.png"
     />
     <img
-      v-if="!recentUpdate"
+      v-if="dateStringtoJsDate(lastUpdated) < lastVisit"
       src=".\..\assets\images\logo\square_dark.png"
     />
     {{ this.projectName }}
@@ -15,14 +15,38 @@
 <script>
 export default {
   name: "AnnouncementWindow",
-  props: ["projectId", "projectName", "recentUpdate"],
+  props: ["projectId", "projectName", "lastUpdated"],
   data: function () {
-    return {};
+    return {
+      lastVisit: new Date()
+    };
   },
   methods: {
     onClick() {
       this.$router.push("/project/" + this.projectId);
     },
+    dateStringtoJsDate(sqlDate){ // TODO: Move to service class
+        //sqlDate in SQL DATETIME format ("yyyy-mm-dd hh:mm:ss.ms")
+        var sqlDateArr1 = sqlDate.split("-");
+        //format of sqlDateArr1[] = ['yyyy','mm','dd hh:mm:ms']
+        var sYear = sqlDateArr1[0];
+        var sMonth = (Number(sqlDateArr1[1]) - 1).toString();
+        var sqlDateArr2 = sqlDateArr1[2].split(" ");
+        //format of sqlDateArr2[] = ['dd', 'hh:mm:ss.ms']
+        var sDay = sqlDateArr2[0];
+        var sqlDateArr3 = sqlDateArr2[1].split(":");
+        //format of sqlDateArr3[] = ['hh','mm','ss.ms']
+        var sHour = sqlDateArr3[0];
+        var sMinute = sqlDateArr3[1];
+        var sqlDateArr4 = sqlDateArr3[2].split(".");
+        //format of sqlDateArr4[] = ['ss','ms']
+        var sSecond = sqlDateArr4[0];
+        var sMillisecond = '000';
+        
+        var date = new Date(sYear,sMonth,sDay,sHour,sMinute,sSecond,sMillisecond);
+
+        return date;
+    }
   },
 };
 </script>
