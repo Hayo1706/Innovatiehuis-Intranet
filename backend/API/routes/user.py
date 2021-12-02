01d243021018b1d00e9c5b87eab72120edfc4aa5
Hayo1706 <43672192+Hayo1706@users.noreply.github.com>
2021-12-02 23:41:07 +0100

    users added to api

--- backend/API/routes/user.py
+++ backend/API/routes/user.py
@@ -5,6 +5,10 @@ def read_all():
     return query("select * from users")
 
 
+def read_one(id):
+    return query("select * from users where userid="+id)
+
+
 def read_user_projects(id):
     user_exists(id)
     return query("SELECT * FROM user_has_projects INNER JOIN projects ON "
