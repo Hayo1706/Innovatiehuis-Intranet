f15baef143635c786bec75d957ee0f655d4925a9
Pieter Beens <beens.pieter@gmail.com>
2021-12-03 00:03:14 +0100

    api url fix

--- intranet-project/src/services/ProjectService.js
+++ intranet-project/src/services/ProjectService.js
@@ -10,29 +10,29 @@ export async function getProjects() {
     return data;
 }
 export async function deleteProject(id) {
-    const { data } = await axiosClient.delete(`/project/${id}`, { timeout: 2000 });
+    const { data } = await axiosClient.delete(`/projects/${id}`, { timeout: 2000 });
     return data;
 }
 
 export async function updateProject(project) {
-    const { data } = await axiosClient.put(`/project/${project.projectid}`, { project }, { timeout: 2000 });
+    const { data } = await axiosClient.put(`/projects/${project.projectid}`, { project }, { timeout: 2000 });
     return data;
 }
 
 export async function getProjectsByUser(userid) {
-    const { data } = await axiosClient.get(`/user/${userid}/projects`, { timeout: 2000 });
+    const { data } = await axiosClient.get(`/users/${userid}/projects`, { timeout: 2000 });
     data.forEach(project => { project.lastupdated = sqlToJsDate(project.lastupdated)});
     return data;
 }
 
 export async function getAnnouncementsByProject(projectid) {
-    const { data } = await axiosClient.get(`/project/${projectid}/announcements`, { timeout: 2000 });
+    const { data } = await axiosClient.get(`/projects/${projectid}/announcements`, { timeout: 2000 });
     data.forEach(announcement => { announcement.timestamp = sqlToJsDate(announcement.timestamp)});
     return data;
 }
 
 export async function postAnnouncement(projectid, announcement) {
-    const { data } = await axiosClient.post(`/project/${projectid}/announcements`, { announcement }, { timeout: 2000 });
+    const { data } = await axiosClient.post(`/projects/${projectid}/announcements`, { announcement }, { timeout: 2000 });
     return data;
 }
 
