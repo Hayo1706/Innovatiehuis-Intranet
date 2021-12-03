f1047a43a835b3bb9aac730d1ecf7d672271d0d0
singh1998 <46492021+singh1998@users.noreply.github.com>
2021-12-03 16:51:39 +0100

    updated endpoint to update project

--- intranet-project/src/services/ProjectService.js
+++ intranet-project/src/services/ProjectService.js
@@ -15,19 +15,19 @@ export async function deleteProject(id) {
 }
 
 export async function updateProject(project) {
-    const { data } = await axiosClient.put(`/projects/${project.projectid}`, { project }, { timeout: 2000 });
+    const { data } = await axiosClient.put(`/projects/${project.projectid}?isarchived=${project.isarchived}`, { timeout: 2000 });
     return data;
 }
 
 export async function getProjectsByUser(userid) {
     const { data } = await axiosClient.get(`/users/${userid}/projects`, { timeout: 2000 });
-    data.forEach(project => { project.lastupdated = sqlToJsDate(project.lastupdated)});
+    data.forEach(project => { project.lastupdated = sqlToJsDate(project.lastupdated) });
     return data;
 }
 
 export async function getAnnouncementsByProject(projectid) {
     const { data } = await axiosClient.get(`/projects/${projectid}/announcements`, { timeout: 2000 });
-    data.forEach(announcement => { announcement.timestamp = sqlToJsDate(announcement.timestamp)});
+    data.forEach(announcement => { announcement.timestamp = sqlToJsDate(announcement.timestamp) });
     return data;
 }
 
