73caa72929c1f03cd99e41659fc9491c73bbaf08
Hayo1706 <43672192+Hayo1706@users.noreply.github.com>
2021-12-06 20:21:56 +0100

    Added api endpoints

--- backend/API/src/routes/users.py
+++ backend/API/src/routes/users.py
@@ -13,3 +13,9 @@ def read_user_projects(id):
     return query("SELECT * FROM user_has_projects INNER JOIN projects ON "
                  "user_has_projects.projectid=projects.projectid"
                  " WHERE userid =  %(id)s AND projects.isarchived = 0", {'id': id})
+
+
+def delete(id):
+    is_int(id)
+    query_update("DELETE FROM users WHERE userid =%(id)s", {'id': id})
+    return make_response("{id} successfully deleted".format(id=str(id)), 200)
