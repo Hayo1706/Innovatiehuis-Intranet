8edb33f2bb9ea0e895f383ab4d035e9bdd37d1b1
singh1998 <46492021+singh1998@users.noreply.github.com>
2021-12-16 16:06:47 +0100

    added Userservice

---
+++ intranet-project/src/services/UserService.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+//import { jsonToJsDate } from './DataConverter';
+
+var UserService = function () {
+    const axiosClient = axios.create({
+        baseURL: 'http://127.0.0.1:5000/api'
+    });
+
+    async function getUsers() {
+        const { data } = await axiosClient.get('/users', { timeout: 2000 });
+        return data;
+    }
+    async function getUsertById(userid) {
+        const { data } = await axiosClient.get(`'/users/${userid}`, { timeout: 2000 });
+        return data;
+    }
+
+    return {
+        getUsers,
+        getUsertById
+    }
+
+}
+export default UserService();
