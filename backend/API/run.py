f8de4dd49ecda8377cf0693842370eaa6f502efa
Hayo1706 <43672192+Hayo1706@users.noreply.github.com>
2021-12-20 15:59:51 +0100

    fix endpoints jcohem yofe ef a

--- backend/API/run.py
+++ backend/API/run.py
@@ -3,8 +3,8 @@ from flask_cors import CORS
 
 app = connexion.App(__name__, specification_dir="./")
 
-# Read the swagger.yml file to configure the endpoints
-app.add_api("swagger.yml")
+# Read the API.yml file to configure the endpoints
+app.add_api("API.yml")
 CORS(app.app)
 if __name__ == "__main__":
     app.run(host='127.0.0.1', debug=True)
