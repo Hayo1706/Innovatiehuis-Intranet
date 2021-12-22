4bda50c42976a32a5b8828bc32039acef90c3af8
Hayo1706 <43672192+Hayo1706@users.noreply.github.com>
2021-12-22 15:47:06 +0100

    singh fix nu de rest

--- backend/API/src/services/JWT.py
+++ backend/API/src/services/JWT.py
@@ -9,32 +9,35 @@ import datetime
 from jose import JWTError, jwt
 
 JWT_ISSUER = 'innovatieplatform'
-JWT_SECRET = 'change_this'  #TODO Change This
+JWT_SECRET = 'change_this'  # TODO Change This
 JWT_LIFETIME_SECONDS = 600
 JWT_ALGORITHM = 'HS256'
 
 ATTEMPTS_BEFORE_COOLDOWN = 6
 COOLDOWN_TIME_SECONDS = 60
 
+
 def generate_token():
     try:
-        dict = connexion.request.json['loginAttempt']
-        email = dict['email']
-        send_password = dict['password']
+        email = connexion.request.form['username']
+        send_password = connexion.request.form['password']
     except KeyError:
         return make_response("Invalid body", 404)
 
-    user = query("SELECT lastwrongpassword,wrongpasswordcount,hash,userid FROM users WHERE email =%(email)s", {'email': email})
+    user = query("SELECT lastwrongpassword,wrongpasswordcount,hash,userid FROM users WHERE email =%(email)s",
+                 {'email': email})
     if len(user) == 0:
         return make_response("Wrong password or username", 401)
     user = user[0]
 
     if int(user['wrongpasswordcount']) >= ATTEMPTS_BEFORE_COOLDOWN:
         if int((datetime.datetime.now() - user['lastwrongpassword']).total_seconds()) > COOLDOWN_TIME_SECONDS:
-            query_update("UPDATE users SET wrongpasswordcount = 0 where userid = %(userid)s", {'userid': user['userid']})
+            query_update("UPDATE users SET wrongpasswordcount = 0 where userid = %(userid)s",
+                         {'userid': user['userid']})
         else:
             return make_response("Acces Denied, your account is blocked for " +
-                                 str(int((datetime.datetime.now() - user['lastwrongpassword']).total_seconds())-ATTEMPTS_BEFORE_COOLDOWN) +
+                                 str(int((datetime.datetime.now() - user[
+                                     'lastwrongpassword']).total_seconds()) - ATTEMPTS_BEFORE_COOLDOWN) +
                                  " more seconds", 401)
     if not user['hash'] == send_password:
         query_update("UPDATE users SET lastwrongpassword = NOW(),wrongpasswordcount = wrongpasswordcount + 1 where "
@@ -47,10 +50,9 @@ def generate_token():
         "iat": int(timestamp),
         "exp": int(timestamp + JWT_LIFETIME_SECONDS),
         "sub": str(id),
-        "scope": str('admin') #TODO Change this
+        "scope": str('admin')  # TODO Change this
     }
-
-    return jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+    return {'access_token': jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM), 'token_type': "bearer"}
 
 
 def decode_token(token):
