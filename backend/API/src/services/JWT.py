2433109c99fdf1cd0144c9e202be16a26f9f720c
Hayo1706 <43672192+Hayo1706@users.noreply.github.com>
2021-12-22 14:20:29 +0100

    wda

--- backend/API/src/services/JWT.py
+++ backend/API/src/services/JWT.py
@@ -9,39 +9,35 @@ import datetime
 from jose import JWTError, jwt
 
 JWT_ISSUER = 'innovatieplatform'
-JWT_SECRET = 'change_this'
+JWT_SECRET = 'change_this'  #TODO Change This
 JWT_LIFETIME_SECONDS = 600
 JWT_ALGORITHM = 'HS256'
 
 ATTEMPTS_BEFORE_COOLDOWN = 6
 COOLDOWN_TIME_SECONDS = 60
 
-
 def generate_token():
     try:
-        dict = connexion.request.json['loginAttempt']
-        email = dict['email']
-        send_password = dict['password']
+        email = connexion.request.form['username']
+        send_password = connexion.request.form['password']
     except KeyError:
         return make_response("Invalid body", 404)
 
-    id = query("SELECT userid FROM users WHERE email =%(email)s", {'email': email})
-    if len(id) == 0:
+    user = query("SELECT lastwrongpassword,wrongpasswordcount,hash,userid FROM users WHERE email =%(email)s", {'email': email})
+    if len(user) == 0:
         return make_response("Wrong password or username", 401)
-    id = id[0]['userid']
-    incorrect = query("SELECT lastwrongpassword,wrongpasswordcount FROM users WHERE userid =%(userid)s", {'userid': id})[0]
+    user = user[0]
 
-    if int(incorrect['wrongpasswordcount']) >= ATTEMPTS_BEFORE_COOLDOWN:
-        if int((datetime.datetime.now() - incorrect['lastwrongpassword']).total_seconds()) > COOLDOWN_TIME_SECONDS:
-            query_update("UPDATE users SET wrongpasswordcount = 0 where userid = %(userid)s", {'userid': id})
+    if int(user['wrongpasswordcount']) >= ATTEMPTS_BEFORE_COOLDOWN:
+        if int((datetime.datetime.now() - user['lastwrongpassword']).total_seconds()) > COOLDOWN_TIME_SECONDS:
+            query_update("UPDATE users SET wrongpasswordcount = 0 where userid = %(userid)s", {'userid': user['userid']})
         else:
             return make_response("Acces Denied, your account is blocked for " +
-                                 str(int((datetime.datetime.now() - incorrect['lastwrongpassword']).total_seconds())-ATTEMPTS_BEFORE_COOLDOWN) +
+                                 str(int((datetime.datetime.now() - user['lastwrongpassword']).total_seconds())-ATTEMPTS_BEFORE_COOLDOWN) +
                                  " more seconds", 401)
-    password = query("SELECT hash FROM users WHERE userid =%(id)s", {'id': id})[0]['hash']
-    if not password == send_password:
+    if not user['hash'] == send_password:
         query_update("UPDATE users SET lastwrongpassword = NOW(),wrongpasswordcount = wrongpasswordcount + 1 where "
-                     "userid = %(userid)s", {'userid': id})
+                     "userid = %(userid)s", {'userid': user['userid']})
         return make_response("Wrong password or username", 401)
 
     timestamp = _current_timestamp()
@@ -50,7 +46,7 @@ def generate_token():
         "iat": int(timestamp),
         "exp": int(timestamp + JWT_LIFETIME_SECONDS),
         "sub": str(id),
-        "scope": str('admin')
+        "scope": str('admin') #TODO Change this
     }
 
     return jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
--- backend/API/src/services/db.py
+++ backend/API/src/services/db.py
@@ -1,3 +1,3 @@
 from sqlalchemy import create_engine
 
-engine = create_engine('mariadb+mariadbconnector://root:admin@127.0.0.1:3306/innovatieplatform',pool_size=5)
+engine = create_engine('mariadb+mariadbconnector://root:admin@127.0.0.1:3306/innovatieplatform',pool_size=10)
