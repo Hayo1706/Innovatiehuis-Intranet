60820ee0ac744a8cb6662999087acd7415dc9bcf
Hayo1706 <43672192+Hayo1706@users.noreply.github.com>
2021-12-27 16:25:04 +0100

    edit users

--- backend/API/src/routes/users.py
+++ backend/API/src/routes/users.py
@@ -4,11 +4,19 @@ from ..services.helper_functions import *
 
 
 def read_all():
-    return query("SELECT userid, firstname, lastname, email, roleid,`name` AS rolename, screeningstatus, createdat, COUNT(projectid) AS amountprojects, IFNULL(MAX(lastseen),createdat) AS lastseen FROM (SELECT * FROM users LEFT JOIN roles USING(roleid)) as users LEFT JOIN users_has_projects USING(userid) GROUP BY userid")
+    return query(
+        "SELECT userid, firstname, lastname, email, roleid,`name` AS rolename, screeningstatus, createdat, "
+        "COUNT(projectid) AS amountprojects, IFNULL(MAX(lastseen),createdat) AS lastseen FROM (SELECT * FROM users "
+        "LEFT JOIN roles USING(roleid)) as users LEFT JOIN users_has_projects USING(userid) GROUP BY userid")
 
 
 def read_one(id):
-    return query("SELECT userid, firstname, lastname, email, roleid,`name` AS rolename, screeningstatus, createdat, COUNT(projectid) AS amountprojects, IFNULL(MAX(lastseen),createdat) AS lastseen FROM (SELECT * FROM users LEFT JOIN roles USING(roleid)) as users LEFT JOIN users_has_projects USING(userid) GROUP BY userid where userid= %(id)s", {'id': id})
+    return query(
+        "SELECT userid, firstname, lastname, email, roleid,`name` AS rolename, screeningstatus, createdat, "
+        "COUNT(projectid) AS amountprojects, IFNULL(MAX(lastseen),createdat) AS lastseen FROM (SELECT * FROM users "
+        "LEFT JOIN roles USING(roleid)) as users LEFT JOIN users_has_projects USING(userid) GROUP BY userid where "
+        "userid= %(id)s",
+        {'id': id})
 
 
 def read_user_projects(id):
@@ -21,6 +29,7 @@ def delete(id):
     is_int(id)
     query_update("DELETE FROM users WHERE userid =%(id)s", {'id': id})
     return make_response("{id} successfully deleted".format(id=str(id)), 200)
+
 
 def post():
     try:
@@ -35,7 +44,27 @@ def post():
         return make_response("Invalid body", 404)
 
     query_update(
-        "INSERT INTO users (firstname, lastname, email, roleid, screeningstatus,hash) VALUES (%(firstname)s, %(lastname)s, %(email)s, %(roleid)s, %(screeningstatus)s,%(hash)s)",
-        {'firstname': firstname, 'lastname': lastname, 'email': email, 'roleid': roleid, 'screeningstatus': screeningstatus, 'hash': hash})
+        "INSERT INTO users (firstname, lastname, email, roleid, screeningstatus,hash) VALUES (%(firstname)s, "
+        "%(lastname)s, %(email)s, %(roleid)s, %(screeningstatus)s,%(hash)s)",
+        {'firstname': firstname, 'lastname': lastname, 'email': email, 'roleid': roleid,
+         'screeningstatus': screeningstatus, 'hash': hash})
     return make_response("User successfully added", 200)
 
+
+def put(id):
+    is_int(id)
+    try:
+        body = connexion.request.json
+        firstname = body['firstname']
+        lastname = body['lastname']
+        email = body['email']
+        roleid = body['roleid']
+        screeningstatus = body['screeningstatus']
+    except KeyError:
+        return make_response("Invalid body", 404)
+
+    query_update(
+        "UPDATE users SET firstname=%(firstname)s, lastname=%(lastname)s, email=%(email)s, roleid=%(roleid)s, screeningstatus=%(screeningstatus)s) WHERE userid=%(id)s",
+        {'firstname': firstname, 'lastname': lastname, 'email': email, 'roleid': roleid,
+         'screeningstatus': screeningstatus, "userid": id})
+    return make_response("User successfully updated", 200)
