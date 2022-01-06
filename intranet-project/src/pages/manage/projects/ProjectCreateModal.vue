e132c84f4a6dcac1a275de9e84e60b04161fa928
singh1998 <46492021+singh1998@users.noreply.github.com>
2022-01-06 14:16:02 +0100

    search for users and select them when creating a new project

--- intranet-project/src/pages/manage/projects/ProjectCreateModal.vue
+++ intranet-project/src/pages/manage/projects/ProjectCreateModal.vue
@@ -34,9 +34,44 @@
               ></textarea>
 
               Leden toevoegen
-              <SearchBar id="userSearchBar"></SearchBar>
+              <br />
+              <span
+                id="selectedUserList"
+                v-for="user in selectedUsers"
+                v-bind:key="user.userid"
+                >{{ user.first_name }} {{ user.last_name }},&nbsp;</span
+              >
+              <SearchBar
+                id="userSearchBar"
+                v-bind:searchTerm="this.userSearchTerm"
+                @searchBarChanged="
+                  (searchTerm) => {
+                    handleSearchUser(searchTerm);
+                  }
+                "
+              ></SearchBar>
+
+              <div class="dropdown-menu" id="userDropdown">
+                <div
+                  class="dropdown-item"
+                  v-for="user in filteredUsers"
+                  v-bind:key="user.userid"
+                  @click="selectUser(user)"
+                >
+                  {{ user.first_name }} {{ user.last_name }}
+                </div>
+              </div>
               Overkoepelende projecten toevoegen
-              <SearchBar id="parentProjectsSearchBar"></SearchBar>
+              <SearchBar
+                id="parentProjectsSearchBar"
+                v-bind:searchTerm="this.parentSearchTerm"
+                @searchBarChanged="
+                  (searchTerm) => {
+                    this.parentSearchTerm = searchTerm;
+                    handleSearchParent(searchTerm);
+                  }
+                "
+              ></SearchBar>
             </div>
           </form>
         </div>
@@ -63,6 +98,7 @@
 </template>
 <script>
 import SearchBar from "@/shared_components/SearchBar.vue";
+import UserService from "@/services/UserService.js";
 export default {
   name: "ProjectCreateModal",
   components: { SearchBar },
@@ -70,6 +106,12 @@ export default {
     return {
       projectname: "",
       projectdescription: "",
+      userSearchTerm: "",
+      parentSearchTerm: "",
+
+      users: [],
+      filteredUsers: [],
+      selectedUsers: [],
     };
   },
   mounted() {
@@ -77,11 +119,72 @@ export default {
     myModalEl.addEventListener("hidden.bs.modal", () => {
       this.clearForm();
     });
+    myModalEl.addEventListener("show.bs.modal", () => {
+      UserService.getUsers()
+        .then((response) => {
+          this.users = response;
+        })
+        .catch((err) => {
+          if (err.response) {
+            console.log(err.response.status);
+          }
+        });
+    });
   },
   methods: {
+    handleSearchUser(name) {
+      if (name) {
+        this.userSearchTerm = name;
+        this.filteredUsers = this.getFilteredUsers();
+      } else {
+        this.filteredUsers = [];
+      }
+    },
+    selectUser(user) {
+      this.userSearchTerm = "";
+      this.filteredUsers = [];
+      this.selectedUsers.push(user);
+      console.log(this.selectedUsers);
+    },
+    getFilteredUsers() {
+      return this.users.filter((item) => {
+        return (
+          (item.first_name + " " + item.last_name)
+            .toLowerCase()
+            .includes(this.userSearchTerm.toLowerCase()) &&
+          !this.selectedUsersContainsUser(item.userid)
+        );
+      });
+    },
+    selectedUsersContainsUser(userid) {
+      for (const user of this.selectedUsers) {
+        if (user.userid == userid) {
+          return true;
+        }
+      }
+      return false;
+    },
+    handleSearchParent(name) {
+      if (name) {
+        console.log(name);
+      }
+    },
     clearForm() {
       this.projectname = "";
       this.projectdescription = "";
+      this.userSearchTerm = "";
+      this.parentSearchTerm = "";
+
+      this.selectedUsers = [];
+    },
+  },
+  watch: {
+    filteredUsers: function () {
+      if (this.filteredUsers.length == 0) {
+        document.getElementById("userDropdown").classList.remove("show");
+      } else {
+        document.getElementById("userDropdown").classList.add("show");
+      }
     },
   },
 };
@@ -107,5 +210,8 @@ textarea {
 #parentProjectsSearchBar {
   margin-top: 5px;
   margin-bottom: 10px;
+}
+#selectedUserList {
+  font-family: AddeleThin;
 }
 </style>
