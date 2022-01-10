a903f8e2c51957379f89aad7143ee29a92c56ff0
Hayo1706 <43672192+Hayo1706@users.noreply.github.com>
2022-01-10 16:18:44 +0100

    Userprofile

--- intranet-project/src/pages/user/PersonalDetails.vue
+++ intranet-project/src/pages/user/PersonalDetails.vue
@@ -1,17 +1,35 @@
 <template>
     <div class="component-container">
       <p>Personal Details</p>
+      <UserListing v-bind:user="this.user"/>
     </div>
+
 </template>
 
 <script>
+import UserListing from "@/pages/manage/users/UserListing";
+import UserService from "@/services/UserService";
 export default {
   name: "PersonalDetails",
+  components: {UserListing},
   props: [
 
   ],
   data: function () {
-    return {};
+    return {
+      user: "",
+    };
+  },
+  async created() {
+    UserService.getUserById(1)
+        .then((response) => {
+          this.user = response;
+        })
+        .catch((err) => {
+          if (err.response) {
+            console.log(err.response.status);
+          }
+        });
   },
 }
 </script>
