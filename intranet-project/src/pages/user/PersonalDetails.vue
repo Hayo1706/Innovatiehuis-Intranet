2bf25bdfd44bba69d5bb04db23972e7d26ba5870
singh1998 <46492021+singh1998@users.noreply.github.com>
2022-01-10 19:44:01 +0100

    made the users page responsive

--- intranet-project/src/pages/user/PersonalDetails.vue
+++ intranet-project/src/pages/user/PersonalDetails.vue
@@ -1,24 +1,48 @@
 <template>
-    <div class="component-container">
-      <div class="row list">
-        <div class="col-6">
-          <div class="item">Naam: </div>
-          <div class="item">Email: </div>
-          <div class="item">Rol: </div>
-          <div class="item">Aanmaakdatum: </div>
-          <div class="item">Laatst gezien: </div>
-          <div class="item">Wachtwoord: </div>
+  <div class="component-container">
+    <div class="row list">
+      <div class="d-block d-lg-none">
+        <div>Naam:</div>
+        <div class="item-text">
+          {{ this.user.first_name }} {{ this.user.last_name }}
         </div>
-        <div class="col-6">
-          <div class="item">{{this.user.first_name}} {{this.user.last_name}}</div>
-          <div class="item">{{this.user.email}}</div>
-          <div class="item">{{this.user.role_name}}</div>
-          <div class="item">{{this.created}}</div>
-          <div class="item">{{(this.last_seen)}}</div>
-          <div class="item"><router-link class="button"  to="/manage/resetpassword">Wachtwoord resetten</router-link></div>
+        <div>Email:</div>
+        <div class="item-text">{{ this.user.email }}</div>
+        <div>Rol:</div>
+        <div class="item-text">{{ this.user.role_name }}</div>
+        <div>Aanmaakdatum:</div>
+        <div class="item-text">{{ this.created }}</div>
+        <div>Laatst gezien:</div>
+        <div class="item-text">{{ this.last_seen }}</div>
+        <div>Wachtwoord:</div>
+        <router-link class="button" to="/manage/resetpassword"
+          >Wachtwoord resetten</router-link
+        >
+      </div>
+      <div class="col-6 d-none d-lg-block">
+        <div class="item">Naam:</div>
+        <div class="item">Email:</div>
+        <div class="item">Rol:</div>
+        <div class="item">Aanmaakdatum:</div>
+        <div class="item">Laatst gezien:</div>
+        <div class="item">Wachtwoord:</div>
+      </div>
+      <div class="col-6 d-none d-lg-block">
+        <div class="item-text">
+          {{ this.user.first_name }} {{ this.user.last_name }}
+        </div>
+        <div class="item-text">{{ this.user.email }}</div>
+        <div class="item-text">{{ this.user.role_name }}</div>
+        <div class="item-text">{{ this.created }}</div>
+        <div class="item-text">{{ this.last_seen }}</div>
+        <div class="item-text">
+          <router-link class="button" to="/manage/resetpassword"
+            >Wachtwoord resetten</router-link
+          >
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -27,9 +51,7 @@ import UserService from "@/services/UserService";
 export default {
   name: "PersonalDetails",
   components: {},
-  props: [
-
-  ],
+  props: [],
   data: function () {
     return {
       user: "",
@@ -38,22 +60,23 @@ export default {
     };
   },
   async created() {
-
     UserService.getUserById(1)
-        .then((response) => {
-          this.user = response[0];
-          this.created = this.user.created.toLocaleString();
-          this.last_seen = this.user.last_seen.toLocaleString();
-          this.$emit('getUserEvent', this.user.first_name + " " + this.user.last_name);
-        })
-        .catch((err) => {
-          if (err.response) {
-            console.log(err.response.status);
-          }
-        });
-
+      .then((response) => {
+        this.user = response[0];
+        this.created = this.user.created.toLocaleString();
+        this.last_seen = this.user.last_seen.toLocaleString();
+        this.$emit(
+          "getUserEvent",
+          this.user.first_name + " " + this.user.last_name
+        );
+      })
+      .catch((err) => {
+        if (err.response) {
+          console.log(err.response.status);
+        }
+      });
   },
-}
+};
 </script>
 
 
@@ -61,13 +84,19 @@ export default {
 .list {
   color: var(--blue1);
 
-  font-size: 20px
-  }
-.item{
+  font-size: 20px;
+}
+.item {
   margin-bottom: 1vh;
   white-space: initial;
 }
-.button{
+.item-text {
+  margin-bottom: 1vh;
+  white-space: initial;
+  font-family: addelleThin;
+}
+
+.button {
   font-size: 15px;
 }
 </style>
