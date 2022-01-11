62b4d3e43f64f94377857beb92b2366b992c71e2
Pieter Beens <beens.pieter@gmail.com>
2022-01-11 17:37:15 +0100

    404 page stylng

--- intranet-project/src/pages/notfound/NotFoundPage.vue
+++ intranet-project/src/pages/notfound/NotFoundPage.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>Sorry! De pagina waar je naar zoekt kan niet gevonden worden!
-    <br>
-    <router-link to="/home" id="golden-button">Terug naar Hoofdpagina</router-link>
+  <div class="component-container" style="margin-left:10vw;">
+    <div class="component-header">
+      Sorry! De pagina waar je naar zoekt kan niet gevonden worden!
+    </div>
+    <div class="component-body">
+      <router-link to="/home" class="full-button" style="width:fit-content">Terug naar Hoofdpagina</router-link>
+    </div>
   </div>
-
-  
 </template>
 
 <script>
@@ -19,14 +21,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-#golden-button {
-  color: aliceblue;
-  background-color: chocolate;
-  border-style: outset;
-  border-radius: 8px;
-  border-spacing: 20px;
-  text-decoration: none;
-}
-</style>
