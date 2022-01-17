695b660492c7b38732516d875e514c44bb07c872
singh1998 <46492021+singh1998@users.noreply.github.com>
2022-01-17 14:19:45 +0100

    create projects and user buttons now work on adminview

--- intranet-project/src/pages/home/AdminView.vue
+++ intranet-project/src/pages/home/AdminView.vue
@@ -5,45 +5,60 @@
     </div>
     <div>
       <button
-          data-toggle="tooltip" data-placement="bottom" title="Projectenoverzicht"
-          class="full-button mobileRow extraLarge"
-          @click="projectsClick()"
-          v-if="this.canSeeProjects()"
-
-      ><img src="../../assets/images/projects_icon.png">Projectenoverzicht</button>
+        data-toggle="tooltip"
+        data-placement="bottom"
+        title="Projectenoverzicht"
+        class="full-button mobileRow extraLarge"
+        @click="projectsClick()"
+        v-if="this.canSeeProjects()"
+      >
+        <img src="../../assets/images/projects_icon.png" />Projectenoverzicht
+      </button>
       <button
-          title="Gebruikersoverzicht"
-          class="full-button mobileRow extraLarge"
-          @click="usersClick()"
-          v-if="this.canSeeUsers()"
-      ><img src="../../assets/images/users_icon.png">Gebruikersoverzicht</button>
-      <hr>
+        title="Gebruikersoverzicht"
+        class="full-button mobileRow extraLarge"
+        @click="usersClick()"
+        v-if="this.canSeeUsers()"
+      >
+        <img src="../../assets/images/users_icon.png" />Gebruikersoverzicht
+      </button>
+      <hr />
       <button
-          title="Gebruikersoverzicht"
-          class="full-button mobileRow extraLarge"
-          @click="usersClick()"
-          v-if="this.canSeeUsers()"
-      >Project aanmaken</button>
+        title="Project aanmaken"
+        class="full-button mobileRow extraLarge"
+        data-bs-target="#createProjectModal"
+        data-bs-toggle="modal"
+        type="button"
+        v-if="this.canCreateProjects()"
+      >
+        Project aanmaken
+      </button>
       <button
-          title="Gebruikersoverzicht"
-          class="full-button mobileRow extraLarge"
-          @click="usersClick()"
-          v-if="this.canSeeUsers()"
-      >Gebruiker toevoegen</button>
+        title="Gebruiker aanmaken"
+        class="full-button mobileRow extraLarge"
+        data-bs-target="#createUserModal"
+        data-bs-toggle="modal"
+        type="button"
+        v-if="this.canCreateUsers()"
+      >
+        Gebruiker toevoegen
+      </button>
     </div>
+    <ProjectCreateModal></ProjectCreateModal>
+    <UserCreateModal></UserCreateModal>
   </div>
 </template>
 
 <script>
 import PermissionService from "@/services/PermissionService";
-
+import ProjectCreateModal from "@/pages/manage/projects/ProjectCreateModal.vue";
+import UserCreateModal from "@/pages/manage/users/UserCreateModal.vue";
 export default {
-  components: {  },
+  components: { ProjectCreateModal, UserCreateModal },
   name: "AdminView",
   props: [],
   data: function () {
-    return {
-    };
+    return {};
   },
   methods: {
     projectsClick() {
@@ -53,25 +68,26 @@ export default {
       this.$router.push("/manage/users");
     },
     canSeeProjects() {
-      return (
-          PermissionService.userHasPermission('may_read_any_project')
-      )
+      return PermissionService.userHasPermission("may_read_any_project");
     },
     canSeeUsers() {
-      return (
-          PermissionService.userHasPermission('may_read_any_user')
-      )
-    }
+      return PermissionService.userHasPermission("may_read_any_user");
+    },
+    canCreateProjects() {
+      return PermissionService.userHasPermission("may_create_project");
+    },
+    canCreateUsers() {
+      return PermissionService.userHasPermission("may_create_users");
+    },
   },
 };
 </script>
 
 <style scoped>
-button:hover{
+button:hover {
   padding-left: 15px;
-
 }
-img{
+img {
   height: 40px;
   margin-right: 20px;
   margin-left: 20px;
