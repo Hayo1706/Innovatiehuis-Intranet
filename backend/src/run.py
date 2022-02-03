77e7f4f4b7d453714037c07e188adfe800f5f196
singh1998 <46492021+singh1998@users.noreply.github.com>
2022-02-03 15:42:21 +0100

    finished backup service for files

--- backend/src/run.py
+++ backend/src/run.py
@@ -1,11 +1,8 @@
 from multiprocessing import Process
 from threading import Thread
 
-from services import app
-
-from src.services.backup import run_backup
+from src import app
 
 if __name__ == "__main__":
-    Thread(target=run_backup).start()
     app.app.run(host='127.0.0.1', debug=False)
 
--- backend/src/services/app.py
+++ backend/src/services/app.py
@@ -1,3 +1,37 @@
+import shutil
+import time
+
+from os import path
+from threading import Thread
+
 from .setup import create_app
+from .. import config
+
 app = create_app()
 
+
+def backup():
+    app.app.backup = True
+    print("Backup started, don't stop the API.")
+
+    time.sleep(10)
+
+    if path.exists(config.BACKUP_ROOT_PATH + "/data"):
+        shutil.rmtree(config.BACKUP_ROOT_PATH + "/data")
+    shutil.copytree(config.FILE_STORAGE_ROOT, config.BACKUP_ROOT_PATH + "/data")
+
+    app.app.backup = False
+    print("Backup complete. "
+          "In " + str(config.BACKUP_FREQUENCY) + " minutes, the backup will start again.")
+
+
+def run_backup():
+    while True:
+        try:
+            time.sleep(config.BACKUP_FREQUENCY * 60)
+            backup()
+        except KeyboardInterrupt:
+            break
+
+
+Thread(target=run_backup).start()
