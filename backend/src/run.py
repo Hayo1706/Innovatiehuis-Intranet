4256729d7c89d525fe75ce7e5581e29636fe76e5
singh1998 <46492021+singh1998@users.noreply.github.com>
2022-02-05 14:34:41 +0100

    not so sure about how this

--- backend/src/run.py
+++ backend/src/run.py
@@ -4,6 +4,6 @@ from threading import Thread
 from src import app
 
 if __name__ == "__main__":
-    # change to 0.0.0.0 when using docker
+    # change to 0.0.0.0 when using docker??
     app.app.run(host='127.0.0.1', debug=False)
 
