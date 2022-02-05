920abb196f18f39b37decbefeeaea6b342bbf0b4
singh1998 <46492021+singh1998@users.noreply.github.com>
2022-02-05 13:17:24 +0100

    added comments for docker and made the sql backup work in docker

--- backend/src/services/app.py
+++ backend/src/services/app.py
@@ -12,7 +12,7 @@ app = create_app()
 app.app.backup = False
 
 DB_HOST = 'localhost'
-DB_USER = 'root'
+DB_USER = 'root@innovatieplatform'
 DB_USER_PASSWORD = 'admin'
 DB_NAME = 'innovatieplatform'
 
@@ -23,13 +23,15 @@ def backup():
 
     time.sleep(10)
 
-    if path.exists(config.BACKUP_ROOT_PATH + "/data"):
-        shutil.rmtree(config.BACKUP_ROOT_PATH + "/data")
-    shutil.copytree(config.FILE_STORAGE_ROOT, config.BACKUP_ROOT_PATH + "/data")
+    if path.exists(config.BACKUP_ROOT_PATH + "data"):
+        shutil.rmtree(config.BACKUP_ROOT_PATH + "data")
+    shutil.copytree(config.FILE_STORAGE_ROOT, config.BACKUP_ROOT_PATH + "data")
 
-    # backup the database
+    # generate sql
     # TODO when in docker, store correctly
-    dumpcmd = "mysqldump -u " + DB_USER + " -p" + " " + DB_NAME + " > " + config.BACKUP_ROOT_PATH + "/sql/" + DB_NAME + ".sql; " + DB_USER_PASSWORD
+    if not path.exists(config.BACKUP_ROOT_PATH+"sql/"):
+        os.mkdir(config.BACKUP_ROOT_PATH+"sql/")
+    dumpcmd = "mysqldump -P 3306 -h 172.28.1.3 -u " + DB_USER + " -p" + DB_USER_PASSWORD + " " + DB_NAME + " > " + config.BACKUP_ROOT_PATH+"sql/" +DB_NAME + ".sql"
     os.system(dumpcmd)
 
     app.app.backup = False
