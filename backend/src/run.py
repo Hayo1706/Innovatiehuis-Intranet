e36d6be8bc5beac048220b9ffb49d957e377b215
singh1998 <46492021+singh1998@users.noreply.github.com>
2022-02-08 13:04:47 +0100

    removed a comment

--- backend/src/run.py
+++ backend/src/run.py
@@ -7,7 +7,6 @@ from src import app
 from src.config import ON_DOCKER
 
 if __name__ == "__main__":
-    # change to 0.0.0.0 when using docker
     if ON_DOCKER:
         waitress.serve(app.app, host='0.0.0.0', port=5000)
     else:
